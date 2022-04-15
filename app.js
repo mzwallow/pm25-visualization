4474438377faef1df29e0b0523d8785f5ff144b9
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2022-04-15 17:29:41 +0000

    :tada: All api is all done

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const XLSX = require('xlsx');
 const app = express();
 const routes = require('./routes/router');
 
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
@@ -46,7 +49,7 @@ app.get('/countries-PM25-gte-50-in-2015.xlsx', async (req, res) => {
 
             XLSX.utils.book_append_sheet(workbook, worksheet, 'PM25>=50');
 
-            XLSX.writeFileXLSX(workbook, 
+            XLSX.writeFileXLSX(workbook,
                 path.join(__dirname, `files/countries-pm25-gte-50-in-2015.xlsx`));
 
             res.sendFile(path.join(__dirname, `files/countries-pm25-gte-50-in-2015.xlsx`),
@@ -80,9 +83,9 @@ app.get('/avg-pm25-by-countries-desc.xlsx', async (req, res) => {
             const worksheet = XLSX.utils.json_to_sheet(result.recordset);
             const workbook = XLSX.utils.book_new();
 
-            XLSX.utils.book_append_sheet(workbook, worksheet, 'PM25>=50');
+            XLSX.utils.book_append_sheet(workbook, worksheet, 'AVG(PM2.5)');
 
-            XLSX.writeFileXLSX(workbook, 
+            XLSX.writeFileXLSX(workbook,
                 path.join(__dirname, `files/avg-pm25-by-countries-desc.xlsx`));
 
             res.sendFile(path.join(__dirname, `files/avg-pm25-by-countries-desc.xlsx`),
@@ -93,7 +96,7 @@ app.get('/avg-pm25-by-countries-desc.xlsx', async (req, res) => {
     })
 });
 
-// List historical PM 2.5 values in given country by year
+// List historical PM 2.5 values in given 'country' by year
 app.get('/:country/historical-pm25-by-year.xlsx', async (req, res) => {
     sql.connect(config, (err) => {
         if (err) {
@@ -116,9 +119,9 @@ app.get('/:country/historical-pm25-by-year.xlsx', async (req, res) => {
             const worksheet = XLSX.utils.json_to_sheet(result.recordset);
             const workbook = XLSX.utils.book_new();
 
-            XLSX.utils.book_append_sheet(workbook, worksheet, 'PM25>=50');
+            XLSX.utils.book_append_sheet(workbook, worksheet, 'Historical PM 2.5');
 
-            XLSX.writeFileXLSX(workbook, 
+            XLSX.writeFileXLSX(workbook,
                 path.join(__dirname, `files/historical-pm25-by-year.xlsx`));
 
             res.sendFile(path.join(__dirname, `files/historical-pm25-by-year.xlsx`),
@@ -153,9 +156,9 @@ app.get('/:year/:color/total-affected-populations', async (req, res) => {
             const worksheet = XLSX.utils.json_to_sheet(result.recordset);
             const workbook = XLSX.utils.book_new();
 
-            XLSX.utils.book_append_sheet(workbook, worksheet, 'PM25>=50');
+            XLSX.utils.book_append_sheet(workbook, worksheet, 'Affected population');
 
-            XLSX.writeFileXLSX(workbook, 
+            XLSX.writeFileXLSX(workbook,
                 path.join(__dirname, `files/total-affected-populations.xlsx`));
 
             res.sendFile(path.join(__dirname, `files/total-affected-populations.xlsx`),
@@ -166,6 +169,176 @@ app.get('/:year/:color/total-affected-populations', async (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server start at port 3000');
+// List all the city points of all countries by given 'year'
+app.get('/city-points-of-all-countries/:year', async (req, res) => {
+    sql.connect(config, (err) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'can not connect to database',
+                error: err
+            });
+        }
+
+        new sql.Request().query(`
+            SELECT country, city, Year, Geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE YEAR = ${req.params['year']}
+        `, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            res.json(result.recordset);            
+        });
+    })
+});
+
+// List the 50 closest city points to Bangkok
+app.get('/50-city-points-closest-to-bangkok', async (req, res) => {
+    sql.connect(config, (err) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'can not connect to database',
+                error: err
+            });
+        }
+
+        new sql.Request().query(`
+            DECLARE @Pol GEOMETRY;
+
+            SELECT @Pol = Geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE city = 'Bangkok';
+
+            SELECT TOP 50 city, Geom.MakeValid().STDistance(@Pol) AS Distance, Geom
+            FROM SpatialDB.dbo.AirPollution
+            ORDER BY Distance ASC;
+        `, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            res.json(result.recordset);            
+        });
+    })
+});
+
+// List all the city points of Thailand’s neighboring countries in 2018
+app.get('/city-points-of-thailand-neighbors-in-2018', async (req, res) => {
+    sql.connect(config, (err) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'can not connect to database',
+                error: err
+            });
+        }
+
+        new sql.Request().query(`
+            SELECT country, year, Geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE country IN ('Cambodia', 'Laos','Myanmar','Malaysia') AND Year = 2018
+        `, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            res.json(result.recordset);            
+        });
+    })
+});
+
+// List the 4 points of MBR covering all city points in Thailand in 2009
+app.get('/4-points-of-mbr-covering-city-points-in-thailand-in-2018', async (req, res) => {
+    sql.connect(config, (err) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'can not connect to database',
+                error: err
+            });
+        }
+
+        new sql.Request().query(`
+            SELECT TOP 1 latitude,longtitude, geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE country = 'Thailand' AND Year = 2014
+            ORDER BY latitude ASC;
+            
+            SELECT TOP 1 latitude,longtitude, geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE country = 'Thailand' AND Year = 2014
+            ORDER BY latitude DESC;
+            
+            SELECT TOP 1 longtitude, latitude, geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE country = 'Thailand' AND Year = 2014
+            ORDER BY longtitude ASC;
+            
+            SELECT TOP 1 longtitude, latitude, geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE country = 'Thailand' AND Year = 2014
+            ORDER BY longtitude DESC;
+        `, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            res.json(result.recordset);            
+        });
+    })
+});
+
+// List all city points of countries having the highest no. of city points in 2011
+app.get('/city-points-of-countries-having-highest-city-points-in-2018', async (req, res) => {
+    sql.connect(config, (err) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'can not connect to database',
+                error: err
+            });
+        }
+
+        new sql.Request().query(`
+            SELECT country, city , Year, geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE country = (SELECT TOP 1 country
+                FROM SpatialDB.dbo.AirPollution
+                GROUP BY country, year HAVING Year = 2011
+                ORDER BY COUNT(country) desc)
+                AND Year = 2011;
+        `, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            res.json(result.recordset);            
+        });
+    })
+});
+
+// List all the city points which are considered as “low income” by given 'year'
+app.get('/city-points-have-low-income/:year', async (req, res) => {
+    sql.connect(config, (err) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'can not connect to database',
+                error: err
+            });
+        }
+
+        new sql.Request().query(`
+            SELECT city , Year, wbinc16_text, geom
+            FROM SpatialDB.dbo.AirPollution
+            WHERE Year = ${req.params['year']} AND wbinc16_text = 'low income';
+        `, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            res.json(result.recordset);            
+        });
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
 });
