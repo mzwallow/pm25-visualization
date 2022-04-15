9db31e01d23abe72cde915b24d21d78561dbb8f3
Natthadecha Kijsamrej <pittaya.sc2@gmail.com>
2022-04-15 21:26:15 +0700

    🔨 add page

--- views/src/App.vue
+++ views/src/App.vue
@@ -2,57 +2,32 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="amber darken-4"
       dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      class="justify-content-center"
+    > 
+    <h2 class="logo">
+      Spatial Database
+    </h2>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-container fluid style="height: 100vh">
+				<router-view></router-view>
+			</v-container>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
+<style scoped>
+  .logo{
+    text-align: center;
+  }
+</style>
 
+<script>
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
     //
   }),
---
+++ views/src/views/my-home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container class="page">
+      <v-card class="card">
+          <v-card-title>ข้อ 4</v-card-title>
+      </v-card>
+      <v-card class="card">
+          <v-card-title>ข้อ 4</v-card-title>
+      </v-card>
+      <v-card class="card">
+          <v-card-title>ข้อ 4</v-card-title>
+      </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+
+	}),
+}
+</script>
+
+<style scoped>
+.page{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px
+}
+.card{
+    padding: 0.5rem;
+    width: 1000px;
+    height: 500px;
+}
+
+</style>
